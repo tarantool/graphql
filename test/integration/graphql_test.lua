tarantool/graphql

commit e37651d4e83d41370a259d053343bec7a1eb083b

support "null" arguments
Before this patch graphql parser doesn't support "null" literal.
Since parser was changed we could support "null" literal
specification.

graphql/util.lua
@@ -66,18 +66,22 @@ local function coerceValue(node, schemaType, variables, opts)
   variables = variables or {}
   opts = opts or {}
   local strict_non_null = opts.strict_non_null or false
-
-  if schemaType.__type == 'NonNull' then
-    local res = coerceValue(node, schemaType.ofType, variables, opts)
-    if strict_non_null and res == nil then
-      error(('Expected non-null for "%s", got null'):format(
-        getTypeName(schemaType)))
-    end
-    return res
-  end
+  local defaultValues = opts.defaultValues or {}
 
   if not node then
     return nil
+  end
+
+  if schemaType.__type == 'NonNull' then
+    if node.kind == 'null' then
+      error(('Expected non-null for "%s", got null'):format(getTypeName(schemaType)))
+    end
+
+    local res = coerceValue(node, schemaType.ofType, variables, opts)
+    if strict_non_null and res == nil then
+      error(('Expected non-null for "%s", got null'):format(getTypeName(schemaType)))
+    end
+    return res
   end
 
   -- handle precompiled values
@@ -86,7 +90,24 @@ local function coerceValue(node, schemaType, variables, opts)
   end
 
   if node.kind == 'variable' then
-    return variables[node.name.value]
+    local value = variables[node.name.value]
+    local defaultValue = defaultValues[node.name.value]
+    if type(value) == 'nil' and type(defaultValue) ~= 'nil' then
+      -- default value was parsed by parseLiteral
+      value = defaultValue
+    elseif schemaType.parseValue ~= nil then
+      value = schemaType.parseValue(value)
+      if strict_non_null and type(value) == 'nil' then
+        error(('Could not coerce variable "%s" with value "%s" to type "%s"'):format(
+            node.name.value, variables[node.name.value], schemaType.name
+        ))
+      end
+    end
+    return value
+  end
+
+  if node.kind == 'null' then
+    return box.NULL
   end
 
   if schemaType.__type == 'List' then
@@ -144,12 +165,11 @@ local function coerceValue(node, schemaType, variables, opts)
   end
 
   if schemaType.__type == 'Scalar' then
-    if schemaType.parseLiteral(node) == nil then
-      error(('Could not coerce "%s" to "%s"'):format(
-        node.value or node.kind, schemaType.name))
+    local value = schemaType.parseLiteral(node)
+    if strict_non_null and type(value) == 'nil' then
+      error(('Could not coerce value "%s" to type "%s"'):format(node.value or node.kind, schemaType.name))
     end
-
-    return schemaType.parseLiteral(node)
+    return value
   end
 end
 

commit 539b5b5b6f79f07f566a99c1dbcef786f6d4aea5

fix validation for non-nullable arguments
The problem was introduced in e37651d4e83d41370a259d053343bec7a1eb083b
(support "null" arguments) here we add redundant check that
started to throw error "graphql/util.lua:72: attempt to index local 'node' (a nil value)"
This patch removes this check and adds test (with small difference
copied from TDG) that catches a problem.

graphql/util.lua
@@ -68,20 +68,16 @@ local function coerceValue(node, schemaType, variables, opts)
   local strict_non_null = opts.strict_non_null or false
   local defaultValues = opts.defaultValues or {}
 
-  if not node then
-    return nil
-  end
-
   if schemaType.__type == 'NonNull' then
-    if node.kind == 'null' then
-      error(('Expected non-null for "%s", got null'):format(getTypeName(schemaType)))
-    end
-
     local res = coerceValue(node, schemaType.ofType, variables, opts)
     if strict_non_null and res == nil then
       error(('Expected non-null for "%s", got null'):format(getTypeName(schemaType)))
     end
     return res
+  end
+
+  if not node then
+    return nil
   end
 
   -- handle precompiled values

test/integration/graphql_test.lua
@@ -1086,3 +1086,46 @@ function g.test_null()
             end
     )
 end
+
+function g.test_validation_non_null_argument_error()
+    local function callback(_, _)
+        return nil
+    end
+
+    local query_schema = {
+        ['TestEntity'] = {
+            kind = types.string,
+            arguments = {
+                insert = types.inputObject({
+                    name = 'TestEntityInput',
+                    fields = {
+                        non_null = types.string.nonNull,
+                    }
+                }),
+            },
+            resolve = callback,
+        }
+    }
+
+    t.assert_error_msg_contains(
+            'Expected non-null',
+            function()
+                check_request([[
+                    query QueryFail {
+                        TestEntity(insert: {})
+                    }
+                ]], query_schema)
+            end
+    )
+
+    t.assert_error_msg_contains(
+            'Expected non-null',
+            function()
+                check_request([[
+                    query QueryFail {
+                        TestEntity(insert: {non_null: null})
+                    }
+                ]], query_schema)
+            end
+    )
+end